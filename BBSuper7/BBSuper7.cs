67b8e44d2c99fa742418cdc78c4c57978267c87d
Administrator <Administrator@USERCHI-V8D6BVF>
2017-11-22 11:33:36 +0800

    添加项目文件。

---
+++ BBSuper7/BBSuper7.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Text;
+using System.Reflection;
+
+using System.Threading;
+using Newtonsoft.Json;
+using WebSocket4Net;
+using Newtonsoft.Json.Linq;
+using System.Windows.Forms;
+
+[assembly: Obfuscation(Feature = "encrypt resources [compress]", Exclude = false)]
+namespace BBSuper7
+{
+    ///------------------------------------------------
+    /// <summary>连环夺宝客户端类</summary>
+    ///------------------------------------------------
+    [Obfuscation(Feature = "Apply to member * when method or constructor: virtualization", Exclude = false)]
+    class BBSuper7
+    {
+        /// <summary>websocket客户端对象</summary>
+        private WebSocket websocket = null;
+        /// <summary>连接状态</summary>
+        public bool ConnectStatus { get { return websocket != null && websocket.State == WebSocketState.Open; } }
+        /// <summary>用户登录名</summary>
+        public string UserLoginName { get; private set; } = string.Empty;
+        /// <summary>用户ID</summary>
+        public int UserId { get; private set; } = 0;
+        /// <summary>大厅ID</summary>
+        public string HallId { get; private set; } = string.Empty;
+        /// <summary>用户金钱</summary>
+        public double UserBalance { get; private set; } = 0;
+        /// <summary>用户分数 初始设定500</summary>
+        public int UserCredit { get; private set; } = 500;
+        /// <summary>等级ID</summary>
+        public int LevelID { get; private set; } = 0;
+        /// <summary>投注总次数</summary>
+        public int BetSum { get; private set; } = 0;
+        /// <summary>流水总和</summary>
+        public int WaterSum { get; private set; } = 0;
+        /// <summary>西瓜数量</summary>
+        public int WatermelonCount { get; private set; } = 0;
+        /// <summary>BAR数量</summary>
+        public int BarCount { get; private set; } = 0;
+        /// <summary>西瓜奖金</summary>
+        public double WatermelonAward { get; private set; } = 0;
+        /// <summary>当前投注比例</summary>
+        public string CurrentBetRatio { get; private set; } = Program.BetRatioA;
+        /// <summary>当前投注注数</summary>
+        public int CurrentBetCount { get; private set; } = Program.BetCount;
+        /// <summary>当前每线投注</summary>
+        public int CurrentLineBet { get; private set; } = Program.LineBet;
+
+
+        ///------------------------------------------------
+        /// <summary>发送消息给远程服务器</summary>
+        ///------------------------------------------------
+        ///
+        [Obfuscation(Feature = "virtualization", Exclude = false)]
+        private void Call(JObject command)
+        {
+            string text = JsonConvert.SerializeObject(command);
+            websocket.Send(text);
+        }
+
+        ///------------------------------------------------
+        /// <summary>接收服务器数据</summary>
+        ///------------------------------------------------
+        ///
+        private void websocket_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            JObject message = JObject.Parse(e.Message);
+            if (message["NetStatusEvent"].ToString() == "NetConnect.Success")
+            {
+                RequestLoginBySid("3a2428402ed932c5a33c0dccacaa031e11f3b3b1");
+            }
+            else if (!string.IsNullOrEmpty((string)message["key"]))
+            {
+                switch ((string)message["key"])
+                {
+                    case "onLoginBySid": ResponseLogin(message); break;
+                    //case "onGetMachineDetail": ResponseGetMachine(message); break;
+                    //case "onCreditExchange": ResponseCreditExchange(message); break;
+                    //case "onCreateFish": ResponseonCreateFish(message); break;
+                    //case "onKillShowgroup": ResponseonKillShowgroup(message); break;
+                    //case "onBulletFire": ResponseBulletFire(message); break;
+                    //case "onKillFish": ResponseonKillFish(message); break;
+                }
+            }
+        }
+        public void RequestLoginBySid(string sid)
+        {
+            JObject loginRequest = new JObject();
+            loginRequest["key"] = "loginBySid";
+            loginRequest["data"] = new JObject();
+            loginRequest["data"]["sid"] = sid;
+            loginRequest["data"]["deviceInfo"] = new JObject();
+            Call(loginRequest);
+        }
+
+        ///------------------------------------------------
+        /// <summary>连接服务器登录游戏</summary>
+        ///------------------------------------------------
+        public void LoginGame()
+        {
+            Program.PrintMessage("开始连接服务器……");
+            websocket = new WebSocket("ws://yhgj.com/fxgm/fxLB?gameType=30599_1");
+            websocket.AllowUnstrustedCertificate = true;
+            websocket.Opened += (object sender, EventArgs e) => { Console.WriteLine("连接服务器成功"); };
+            websocket.Closed += (object sender, EventArgs e) => { Console.WriteLine("连接关闭"); };
+            websocket.Error += (object sender, SuperSocket.ClientEngine.ErrorEventArgs e) => { Console.WriteLine(e.Exception.ToString()); };
+            websocket.MessageReceived += websocket_MessageReceived;
+            websocket.Open();
+        }
+
+        ///------------------------------------------------
+        /// <summary>请求通过会话ID登录游戏</summary>
+        ///------------------------------------------------
+        //public void RequestLoginBySid(string sid)
+        //{
+        //    JObject loginRequest = new JObject();
+        //    loginRequest["command"] = "loginBySid";
+        //    loginRequest["args"] = new JArray();
+        //    (loginRequest["args"]as JArray).Add(sid);
+        //    (loginRequest["args"] as JArray).Add("5061");
+        //    (loginRequest["args"] as JArray).Add("{\"rd\":\"rd1\",\"ua\":\"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36\",\"os\":\"Windows 7\",\"srs\":\"2048x1152\",\"wrs\":\"1044x778\",\"dpr\":1.25,\"pl\":\"H5\",\"pf\":\"Chrome 62.0.3202.89\",\"wv\":\"false\",\"aio\":false,\"vga\":\"ANGLE (AMD Radeon (TM) R9 Fury Series Direct3D11 vs_5_0 ps_5_0)\",\"tablet\":false,\"cts\":1510546111064,\"mua\":\"\",\"dtp\":\"\"}"); 
+        //    Call(loginRequest);
+        //}
+
+        ///------------------------------------------------
+        /// <summary>登录游戏响应</summary>
+        ///------------------------------------------------
+        public void ResponseLogin(JObject message)
+        {
+            // {"command":"takeMachine","args":["0"]}
+            JObject loadInfoRequest = new JObject();
+            loadInfoRequest["command"] = "takeMachine";
+            loadInfoRequest["args"] = new JArray();
+            (loadInfoRequest["args"] as JArray).Add("0");
+            Call(loadInfoRequest);
+        }
+
+        ///------------------------------------------------
+        /// <summary>请求载入信息</summary>
+        ///------------------------------------------------
+        public void RequestLoadInfo()
+        {
+            JObject loadInfoRequest = new JObject();
+            loadInfoRequest["command"] = "onLoadInfo2";
+            Call(loadInfoRequest);
+        }
+
+        ///------------------------------------------------
+        /// <summary>请求获取兑换比例</summary>
+        ///------------------------------------------------
+        public void RequestOnGetMachineDetail()
+        {
+            JObject loadInfoRequest = new JObject();
+            loadInfoRequest["command"] = "getMachineDetail";
+            Call(loadInfoRequest);
+        }   
+
+        ///------------------------------------------------
+        /// <summary>请求兑换分数</summary>
+        ///------------------------------------------------
+        public void RequestCreditExchange(string betRatio, int credit)
+        {
+            JObject creditExchangeRequest = new JObject();
+            creditExchangeRequest["command"] = "creditExchange";
+            creditExchangeRequest["args"] = new JArray();
+            (creditExchangeRequest["args"] as JArray).Add(betRatio);//"1:10"
+            (creditExchangeRequest["args"] as JArray).Add("500");
+            Call(creditExchangeRequest);
+        }
+        
+        ///------------------------------------------------
+        /// <summary>请求结算分数</summary>
+        ///------------------------------------------------
+        public void RequestBalanceExchange()
+        {
+            JObject balanceExchangeRequest = new JObject();
+            balanceExchangeRequest["command"] = "balanceExchange";
+            Call(balanceExchangeRequest);
+        }
+
+        ///------------------------------------------------
+        /// <summary>请求开始游戏</summary>
+        ///------------------------------------------------
+        public void RequestBeginGame()
+        {
+            // 如果西瓜数量为1, 并且西瓜分值大于等于X分, 则退出游戏
+            if (WatermelonCount == 1&& WatermelonAward >= Program.WatermelonAwardTemplate)
+            {
+                if (websocket.State == WebSocketState.Open)
+                {
+                     websocket.Close();
+                     websocket.Dispose();
+                }
+            }
+            
+            // 如果西瓜数量为1, 则以72分(满注80分)开始转动
+            if (WatermelonCount == 1)
+            {
+                CurrentLineBet = 9;
+                CurrentBetCount = 8;
+            }
+
+            // 西瓜分值高于X, 则切换到B比例,转一次后西瓜分值会回到初始300
+            if (WatermelonAward > Program.WatermelonAwardTemplate)
+            {
+                Program.PrintMessage("切换到B比例"); 
+                CurrentBetRatio = Program.BetRatioB;
+                RequestOnGetMachineDetail();
+            }
+            else if(UserCredit < 80)//剩余积分不足
+            {
+                RequestOnGetMachineDetail();
+            }else
+            {
+                JObject beginGameRequest = new JObject();
+                beginGameRequest["command"] = "beginGame2";
+                beginGameRequest["args"] = new JArray();
+                (beginGameRequest["args"] as JArray).Add(Program.SessionId);
+                (beginGameRequest["args"] as JArray).Add(CurrentLineBet.ToString());
+                (beginGameRequest["args"] as JArray).Add(CurrentBetCount.ToString());
+                Call(beginGameRequest);
+            }
+        }
+        
+        ///------------------------------------------------
+        /// <summary>请求结束游戏</summary>
+        ///------------------------------------------------
+        public void RequestEndGame(string wagersID)
+        {
+            JObject endGameRequest = new JObject();
+            endGameRequest["command"] = "endGame";
+            endGameRequest["args"] = new JArray();
+            (endGameRequest["args"] as JArray).Add(Program.SessionId);
+            (endGameRequest["args"] as JArray).Add(wagersID);
+            Call(endGameRequest);
+        }
+
+        ///------------------------------------------------
+        /// <summary>服务器准备就绪</summary>
+        ///------------------------------------------------
+        public void ResponseReady(JObject message)
+        {
+            // {"proxied":true,"error":"","amf":{"IsFlex":false,"Name":"_result","TransactionID":1,"Objects":[{"capabilities":127,"fmsVer":"FMS/3,5,7,7009","mode":1},{"code":"NetConnection.Connect.Success","data":{"version":"3,5,7,7009"},"description":"Connection succeeded.","level":"status","objectEncoding":0}],"Error":null}}
+            if (message["amf"]["Objects"].ToString().IndexOf("succeeded") >-1)
+            {
+                RequestLoginBySid(Program.SessionId);
+            }
+        }
+        
+        ///------------------------------------------------
+        /// <summary>登记桌台响应</summary>
+        ///------------------------------------------------
+        public void ResponseTakeMachine(JObject message)
+        {
+            // {"proxied":true,"error":"","amf":{"IsFlex":false,"Name":"onTakeMachine","TransactionID":0,"Objects":[null,{"data":{"gameCode":1},"event":true}],"Error":null}}
+            RequestLoadInfo();
+        }
+
+        ///------------------------------------------------
+        /// <summary>获取兑换比例</summary>
+        ///------------------------------------------------
+        public void ResponseOnGetMachineDetail(JObject message)
+        {
+            // {"proxied":true,"error":"","amf":{"IsFlex":false,"Name":"onTakeMachine","TransactionID":0,"Objects":[null,{"data":{"gameCode":1},"event":true}],"Error":null}}
+            RequestBalanceExchange();
+        }
+
+        ///------------------------------------------------
+        /// <summary>载入信息响应</summary>
+        ///------------------------------------------------
+        public void ResponseLoadInfo(JObject message)
+        {
+            //{"proxied":true,"error":"","amf":{"IsFlex":false,"Name":"onOnLoadInfo2","TransactionID":0,"Objects":[null,{"data":{"Balance":478.8,"Base":"1:10,1:5,1:2,1:1,2:1,5:1,10:1","Bell3Times":"1","BetBase":"","BetTotal":"1950534.5000","Cherry3Times":"4","Credit":"0.00","Currency":"RMB","DefaultBase":"1:1","Evolution7":"","Evolution7Lock":false,"ExchangeRate":"1","HallID":3.819831e+06,"LoginName":"d496f146689","MaxLineBet":10,"PayTotal":"1894119.7780","Percent":"99","PercentLow":"96","Status":"N","Test":false,"ThreeBar3Times":"10","UserID":1.49237885e+08,"WagersID":"0","Watermelon3Times":"6","event":true,"isCash":false},"event":true}],"Error":null}} 
+            UserLoginName = (string)message["amf"]["Objects"][1]["data"]["LoginName"];
+            UserBalance   = (double)message["amf"]["Objects"][1]["data"]["Balance"];
+            WatermelonCount  = (int)message["amf"]["Objects"][1]["data"]["Watermelon3Times"];
+            BarCount         = (int)message["amf"]["Objects"][1]["data"]["ThreeBar3Times"];
+            Program.PrintMessage(string.Format("登录游戏成功, 西瓜数量:{0}, BAR数量:{1}", WatermelonCount, BarCount));
+
+            //判断西瓜数量及BAR数量是否为指定值，则以A比例入场，反之关闭连接, 新建连接继续扫描桌台
+            if (WatermelonCount == Program.WatermelonTemplate && BarCount == Program.BarTemplate)
+            {
+            //if (WatermelonCount < Program.WatermelonTemplate && BarCount < Program.BarTemplate) //for test
+            //{
+                RequestCreditExchange(CurrentBetRatio, UserCredit);
+                Program.PrintMessage(string.Format("登录游戏成功, 用户名:{0}, 用户ID:{1}, 账户余额:{2:F}", UserLoginName, UserId, UserBalance));
+            }
+            else
+            {
+                if (websocket.State == WebSocketState.Open)
+                {
+                    websocket.Close();
+                    websocket.Dispose();
+                }
+            }
+        }
+
+        ///------------------------------------------------
+        /// <summary>兑换分数响应</summary>
+        ///------------------------------------------------
+        public void ResponseCreditExchange(JObject message)
+        {
+            // {"proxied":true,"error":"","amf":{"IsFlex":false,"Name":"onCreditExchange","TransactionID":0,"Objects":[null,{"data":{"Balance":428.8,"Base":"1:10","Bell3Times":"5","BetBase":"1:10","Cherry3Times":"2","Credit":500,"Evolution7":"","Evolution7Lock":"","Super7_AB":"20614.04","Super7_AB_Temp":900,"Super7_AE":"42772.08","Super7_AE_Temp":3000,"Super7_AW":"2194.68","Super7_AW_Temp":300,"ThreeBar3Times":"7","Watermelon3Times":"13","event":true},"event":true}],"Error":null}}
+            UserCredit = (int)message["amf"]["Objects"][1]["data"]["Credit"];
+            UserBalance = (double)message["amf"]["Objects"][1]["data"]["Balance"];
+            WatermelonAward =double.Parse(message["amf"]["Objects"][1]["data"]["Super7_AW_Temp"].ToString());
+            Program.PrintMessage(string.Format("兑换分数成功, 用户分数:{0:F}, 账户余额:{1:F}", UserCredit, UserBalance));
+            RequestBeginGame();
+        }
+
+        ///------------------------------------------------
+        /// <summary>结算分数响应</summary>
+        ///------------------------------------------------
+        public void ResponseBalanceExchange(JObject message)
+        {
+            // {"proxied":true,"error":"","amf":{"IsFlex":false,"Name":"onBalanceExchange","TransactionID":0,"Objects":[null,{"data":{"Amount":49.9,"Balance":479.1,"BetBase":"","TransCredit":"499.00","event":true},"event":true}],"Error":null}}
+            UserBalance = (double)message["amf"]["Objects"][1]["data"]["Balance"];
+            Program.PrintMessage(string.Format("结算分数成功, 账户余额:{0:F}", UserBalance));
+            RequestCreditExchange(CurrentBetRatio, UserCredit);
+        }
+
+        ///------------------------------------------------
+        /// <summary>开始游戏响应</summary>
+        ///------------------------------------------------
+        public void ResponseBeginGame(JObject message)
+        {
+            //处理BET_AT_THE_SAME_TIME
+            if (message["amf"]["Objects"][1]["error"]!=null&&message.ToString().IndexOf("SAME_TIME")>-1)
+            {
+                RequestBeginGame();
+            }
+            else
+            {
+                //刷新西瓜数量、西瓜分数、BAR数量
+                WatermelonCount = (int)message["amf"]["Objects"][1]["data"]["Watermelon3Times"];
+                WatermelonAward = (double)message["amf"]["Objects"][1]["data"]["Super7_AW_Temp"];
+                BarCount = (int)message["amf"]["Objects"][1]["data"]["ThreeBar3Times"];
+                UserCredit = (int)(double.Parse(message["amf"]["Objects"][1]["data"]["Credit"].ToString()));
+                Program.PrintMessage(string.Format("开始游戏成功,用户分数:{0:F}", UserCredit));
+
+                if (!string.IsNullOrEmpty(message["amf"]["Objects"][1]["data"]["FreeGame"].ToString()))//判断是否有免费自动翻滚,WagersID不同
+                {
+                    RequestEndGame((string)message["amf"]["Objects"][1]["data"]["FreeGame"]["BonusInfo"][0]["WagersID"]);
+                }
+                else
+                {
+                    RequestEndGame((string)message["amf"]["Objects"][1]["data"]["WagersID"]);
+                }
+            }
+        }
+
+        ///------------------------------------------------
+        /// <summary>结束游戏响应</summary>
+        ///------------------------------------------------
+        public void ResponseEndGame(JObject message)
+        {
+            // {"action":"onEndGame","result":{"event":true,"data":{"Credit":"306.00","GameCode":18}}}
+            //处理多次发结束包导致的异常
+            if (message["amf"]["Objects"][1]["error"]!=null&&message["amf"]["Objects"][1]["error"].ToString().IndexOf("ENDED") > -1)
+            {
+                RequestBeginGame();
+            }
+
+            UserCredit = (int)(double.Parse(message["amf"]["Objects"][1]["data"]["Credit"].ToString()));
+            WaterSum = WaterSum + CurrentLineBet * CurrentBetCount;
+            Program.PrintMessage(string.Format("结束游戏成功, 用户分数:{0:F}, 投注流水:{1:F}", UserCredit, WaterSum));
+            if (CurrentBetRatio == Program.BetRatioB) //B比例转一圈后, 切回A比例继续转动
+            {
+                Program.PrintMessage("切换回A比例");
+                CurrentBetRatio = Program.BetRatioA;
+                RequestOnGetMachineDetail();
+            }else
+            {
+                RequestBeginGame();
+            }
+        }
+    }
+}
+
---
+++ BBSuper7/Program.cs
@@ -0,0 +1,118 @@
+﻿#define KSS
+using System;
+using System.IO;
+using System.Threading;
+using System.Reflection;
+using CustomTools;
+using CustomTools.DialogTools;
+using System.Windows.Forms;
+
+
+#if KSS
+[assembly: Obfuscation(Feature = "merge with CopyrightProtected.dll", Exclude = false)]
+#endif
+[assembly: Obfuscation(Feature = "merge with CustomTools.dll", Exclude = false)]
+[assembly: Obfuscation(Feature = "merge with Newtonsoft.Json.dll", Exclude = false)]
+[assembly: Obfuscation(Feature = "encrypt symbol names with password Spider", Exclude = false)]
+[assembly: Obfuscation(Feature = "encrypt resources", Exclude = false)]
+namespace BBSuper7
+{
+    [Obfuscation(Feature = "Apply to member * when method or constructor: virtualization", Exclude = false)]
+    class Program
+    {
+        /// <summary>游戏链接</summary>
+        public static string GameUrl { get; private set; } = string.Empty;
+        /// <summary>会话ID</summary>
+        public static string SessionId { get; private set; } = string.Empty;
+        /// <summary>携带金钱</summary>
+        public static double CarryBalance { get; private set; } = 0;
+        /// <summary>投注注数</summary>
+        public static int BetCount { get; private set; } = 0;
+        /// <summary>每线投注</summary>
+        public static int LineBet { get; private set; } = 0;
+        /// <summary>西瓜数量指定值</summary>
+        public static int WatermelonTemplate { get; private set; } = 10;
+        /// <summary>西瓜分值指定值</summary>
+        public static double WatermelonAwardTemplate { get;  set; } = 500;
+        /// <summary>BAR数量指定值</summary>
+        public static int BarTemplate { get; private set; } = 10;
+        /// <summary>A投注比例</summary>
+        public static string BetRatioA { get; private set; } = "1:10";
+        /// <summary>B投注比例</summary>
+        public static string BetRatioB { get; private set; } = "1:5";
+    
+        ///------------------------------------------------
+        /// <summary>打印消息文本, 并且保存消息文本到磁盘</summary>
+        ///------------------------------------------------
+        public static void PrintMessage(string msg)
+        {
+            string time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            msg = string.Format("{0}--->{1}", time, msg);
+            Console.WriteLine(msg);
+            StreamWriter writer = new StreamWriter("message.txt", true);
+            writer.WriteLine(msg);
+            writer.Close();
+        }
+
+        ///------------------------------------------------
+        /// <summary>程序入口函数</summary>
+        ///------------------------------------------------
+        static void Main(string[] args)
+        {
+#if DEBUG
+            BetCount = 1;
+            LineBet = 1;
+            CarryBalance = 100;
+            GameUrl = "ws://777.acsstech.com/websocket/SlotMachine/service.mob";
+            //SessionId from Cookie
+            SessionId = "a8e29fc76d92b3679c9f01e24a43111c9b4825ed";
+#else
+#if KSS
+            MiniDatabase config = new MiniDatabase();
+
+            // 初始化验证参数
+            string soft_code = "1000006";
+            string soft_key = "tBH4ri98Q8Vl8YF2D23NR5zk";
+            string rsamod = @"87511866381601393955758054040600923683586568809125777612870390917937184444201";
+            string rsapubkey = @"65537";
+            string sign_data = @"e8dee41144940964fa31f090abd2e61d94865a644480685d0085929ed339522e02427e0ca6f8c228433efc66e66df5dc6c79519594933538182c92e4004a70ac8ea9285c540a1080e2f7f4942cef6eefc0136ce2544c9908d19a4f0d20704e145143625c0eed52b055d5fc67fe607ee12663af8478737453c797bc7389c6f04a1fe448de4c7021f72c790a444ca54f3d0e58259034c75e8e4db6d9fdaf8ea240d3e51c0b468bda17ef213dd1a66ccb8a5e42f5771bc3b9db11ec48cf1a11230961a3b787a42c19e458b142d2328fc2ebb34c26b30d4f9f5f2629c783bff7a8840135a07f5a121c34df23c24ec2d85146800f1f732f321faabf247c009ce129de352a8030454f64604e35852215ec53bf853cf900308c8c506396a2b8651b1f110364f610fe7a7cb339b7da2d1a82b0afe23aed10fd93d85f1ab78c6f4d559cc393a882be0bb36ad694ace47bfd6880bfebde209f33e2989b4ee90598425bad94f6c033f7b9fed6b3a388dfdcb21daaa65987dd8dac98b55077ff8c1160b3a0442bb565bff5b82d856c624aa5d59d5b1f";
+            string license_key = @"Be1kSDgKIKSKpa/C6mBkYY05JLD9BQEiu4KX2ZL7PJ0Rd0WnT258sOB0QqsOvmmOFVAAh0Jmb12sKPBquTF7Xy1/CbnQ1t8Z+VkgVvJXKvA0OQ4yYXTgTMNRJYcLUb+s7JhqEEoPyR6S+e8ITDThwQqFs8bZQ0U7Q3niU0vevSuf7907U837CW6eIg0EUeNlEUoTZ0aMm8p/LzEM6FgD6lreJb7+p6yNABG/237pf5BDrWiHkb7vzdiUGA6roawaraLLAvVcr+qUpaeRuxwOJNiNUhGL5L6M25aPRhCvkxPzZZPCq0/S1Jd9tSZbfU7xQAeAefOSyQdECGQGkx99E1tkslNbejY5cYLrj5ECR3L1q20LRnj04WckfdXLDC2sZSoI/IgJpvvjGijS1EJbaXT+7UsXtGV3bkwCH92H+e2C6h1zeB7Xr43fHAcFgmJuY+2+Vc/zFU/6dOXN0rrM/+U1uudmDnQ2Sw99Mq5gjTR6HyFYoe9EsdNKpZotOQebDl1YvAzaSnjmUyKYS2DB4gLPmKXgluYQBZyabPdUabkEGnScUUctPVOIVfEPw9hBoE9yucnwCyTYzN2dcI5b55ia68j/X7ReN/nVB9zFyeyo3gZmfcLXuk0pgsnkxFpSJ4+ITC9lw2GZUiqIFmeyPVhGHynskteAoBoWxHfbI7CgC6biS9dbbjH1KAuUkYNov86Ygv26Mkku2swJBgluWSnzcyj9KET82R+Ts0+OW6g8g+TOVxlAMqPfSoGT0/WJq2C2tNYBv4e9W4jwz2HKFHfqw0y3AS1Fvf9KuD0Ua9hdVQf38sg5Bz575xOhcYYf++Hd8RXZimR7mj0qkO1f29GCYehFgEqzNKpnKzBkrUkOfnBMATHqvCRUsxJ0Ya/K1ZppRgo7uCP0113DlosFyPwq2y65GmV6F02OuZcP2OdJGcBE0zFbDLXyWURG+nwc28gy+vmizQZeb+n7WtDdlw/5isuhimejZ6J/j0W5xtAlqwK/gKSHzOAa0r77h72PGMa5bt+WvxsNpz3SeOLKc/FErDmtaKQuKH/y322nwe2D9SG/Vz+LnFzx0wB6JD22s5nqi2z6VqF+agVanIpskRyloze4FlmqHTPyEYsfriYMK8vCg2EENJUByAARLZ8r7E/n3qoK8b00KB+ic3Hm0fFnQaFEJD9RnnjWLEp32xU2zyBBd6RUpHrJR7Nj6lOB2DA8IaQ3o30uUBclTvKuRxDxVTOE81ejmZR23iBlOKDW6330DzXVj/4TxCrFoX9Usm+5phqRK1FUwOJw83XSo8cEdDs1Cz5coumNkRqhjs9uJSsaPCoJ1l5D/iUYd/9nNKeTph9BY4HQ4+mlciz+n5HNbp3xIlTTw/N+nyIRgfyVl1KOeRJaovJjNzpSSB0yLpC3WXGAsTDm8ndH6sOAd9k2SQURTnyWZPmq7gU0gM7AkY6UqZABGmptnbPvn2hy5TvcQ2sY0s0ym0lwfucM24k7+XtObAGFw3c2piLeMmkUxelyc0z/Pn7lRfCz70SpeRvuYTGJZ7iHrXXsy09tMrqM27jvw792GZtXW1cO9Pgmmk1SsnAoClmO3SmxIQc4DdSMSXmO8AQtO4GxcvVfdKWxRh/RvcpOOUtLNwTUZ5sYeIAMmof68bwt0GNUC4PCzw+30pFSxo8P7qSqrIuj/gNQ6hjm+cM0DuRJkP6UJPE=";
+            kssPlugin kss_object = new kssPlugin(soft_code, soft_key, rsamod, rsapubkey, sign_data, license_key);
+            
+
+            // 从配置文件中读取注册卡号
+            string card_number = config["运行参数", "注册卡号"];
+
+            // 验证注册卡号是否有效
+            while (card_number.Length == 0 || kss_object.card_number_login(card_number, 1, string.Empty) != string.Empty)
+            {
+                // 请求用户输入注册卡号
+                InputTextDialog dialog_object = new InputTextDialog("卡号输入", "注册卡号:");
+                DialogResult dialog_result = dialog_object.ShowDialog();
+                if (dialog_result == DialogResult.OK)
+                    card_number = dialog_object.InputText;
+                else
+                    System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+
+            // 将注册卡号保存至配置文件中
+            config["运行参数", "注册卡号"] = card_number;
+#endif
+            //WatermelonTemplate = 10;
+            //BarTemplte = 10;
+            BetCount = int.Parse(config["运行参数", "投注注数"]);
+            LineBet = int.Parse(config["运行参数", "线注金额"]);
+            CarryBalance = int.Parse(config["运行参数", "携带金钱"]);
+            GameUrl = config["运行参数", "游戏链接"];
+            SessionId = config["运行参数", "游戏会话"];
+#endif
+            // if (BetCount >= 1 && BetCount <= 5 && LineBet >= 10 && LineBet <= 200)
+            BBSuper7 gameClient = new BBSuper7();
+            while (true)
+            {
+                if (gameClient.ConnectStatus == false) gameClient.LoginGame();
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
